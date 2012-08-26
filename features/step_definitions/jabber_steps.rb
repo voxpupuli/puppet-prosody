352761569d02b1a8bb33aa91a25e453232851824
R. Tyler Croy <tyler@monkeypox.org>
2012-08-26 15:30:31 -0700

    Basic Blimpy integration for kick-starting a Puppet-capable VM on AWS for integration tests

--- features/step_definitions/jabber_steps.rb
+++ features/step_definitions/jabber_steps.rb
@@ -1,13 +1,34 @@
 Given /^I have an empty Linux machine$/ do
-  pending # express the regexp above with the code you wish you had
+  @fleet = Blimpy.fleet do |fleet|
+    fleet.add(:aws) do |ship|
+      ship.name = "prosody-cucumber"
+      # Ubuntu 12.04 LTS in us-west-2
+      ship.image_id = 'ami-4038b470'
+      ship.region = 'us-west-2'
+      ship.livery = Blimpy::Livery::Puppet
+    end
+  end
+
+  # Make sure we set up our object properly
+  expect(@fleet).to_not be_nil
 end
 
 Given /^the machine should become a Jabber server$/ do
-  pending # express the regexp above with the code you wish you had
+  resources << "class { 'prosody' : ; }"
 end
 
 When /^I provision the host$/ do
-  pending # express the regexp above with the code you wish you had
+  resources << "group { 'puppet' : ensure => present; }"
+
+  File.open(File.join(manifest_path, 'site.pp'), 'w') do |f|
+    f.write("node default {\n")
+    resources.each do |r|
+      f.write("#{r}\n")
+    end
+    f.write("}\n")
+  end
+
+  @fleet.start
 end
 
 Then /^the Jabber server should be running$/ do
---
+++ features/support/env.rb
@@ -0,0 +1,63 @@
+require 'fileutils'
+
+require 'rubygems'
+require 'blimpy'
+
+
+module Prosody
+  module World
+    def resources
+      # Resources should be an Array of strings that will be joined together to
+      # make the full Puppet node manifest that will be provisioned on the host
+      @resources ||= []
+    end
+
+    def work_dir
+      File.expand_path(File.dirname(__FILE__) + "/../../tmp/cucumber")
+    end
+
+    def manifest_path
+      File.join(work_dir, 'manifests')
+    end
+
+    def modules_path
+      File.join(work_dir, 'modules')
+    end
+
+    def setup_work_dir
+      # We can't set this up unless we've already "remembered" our original
+      # directory
+      expect(@original_dir).to_not be_nil
+
+      FileUtils.mkdir_p(manifest_path)
+      FileUtils.mkdir_p(modules_path)
+      FileUtils.ln_s(@original_dir, File.join(work_dir, 'modules', 'prosody'))
+    end
+  end
+end
+
+
+World(Prosody::World)
+
+
+Before do
+  @original_dir = Dir.pwd
+
+  unless File.exists?(work_dir)
+    FileUtils.mkdir_p(work_dir)
+  end
+
+  Dir.chdir(work_dir)
+
+  setup_work_dir
+end
+
+After do
+  unless @fleet.nil?
+    @fleet.destroy
+  end
+
+  Dir.chdir(@original_dir)
+  # Nuke the temporary working directory after we're all finished
+  FileUtils.rm_rf(work_dir)
+end
