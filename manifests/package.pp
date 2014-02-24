254c9a7c98d051eddc591f6360d85e903f599538
Franz Pletz <fpletz@fnordicwalking.de>
2014-02-24 21:39:43 +0100

    Install lua-event for libevent support

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,11 +4,15 @@ class prosody::package {
     fail("Currently the prosody module only supports Ubuntu\nSee <https://github.com/rtyler/puppet-prosody> for more")
   }
 
-  package {
-    'prosody' :
-      ensure  => present,
-      # Package[openssl] will be required to generate certificates and such
-      require => Package[openssl];
+  # for libevent support
+  package { 'lua-event':
+    ensure => present,
+  } ->
+
+  package { 'prosody' :
+    ensure  => present,
+    # Package[openssl] will be required to generate certificates and such
+    require => Package[openssl];
   }
 
   if (!defined(Package[openssl])) {
