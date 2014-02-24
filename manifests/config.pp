68b3b4bd3ef53667f2748e075bfdc2920e990423
Franz Pletz <fpletz@fnordicwalking.de>
2014-02-24 21:39:20 +0100

    Add global prosody.cfg support

---
+++ manifests/config.pp
@@ -0,0 +1,10 @@
+class prosody::config {
+  file { '/etc/prosody/conf.avail':
+    ensure => directory,
+  }
+
+  file { '/etc/prosody/prosody.cfg.lua':
+    content => template('prosody/prosody.cfg.erb'),
+    notify  => Service['prosody'],
+  }
+}
