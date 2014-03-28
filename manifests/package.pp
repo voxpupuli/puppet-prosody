7e968d300d1d0bfd82bb0811e8190d970a54e933
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-28 14:36:43 -0700

    Removing libevent package (bugfix)
    Prosody automatically includes the necessary libevent package when it is
    installed.

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,11 +4,6 @@ class prosody::package {
     fail("Currently the prosody module only supports Ubuntu\nSee <https://github.com/rtyler/puppet-prosody> for more")
   }
 
-  # for libevent support
-  package { 'lua-event':
-    ensure => present,
-  } ->
-
   package { 'prosody' :
     ensure  => present,
     # Package[openssl] will be required to generate certificates and such
