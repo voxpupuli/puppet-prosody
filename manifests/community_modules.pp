593ea675112098b7b142662feed96f27afd4b291
Franz Pletz <fpletz@fnordicwalking.de>
2014-10-27 20:11:09 +0100

    Add new parameters source & revision for class community_modules

--- manifests/community_modules.pp
+++ manifests/community_modules.pp
@@ -1,12 +1,15 @@
 class prosody::community_modules (
   $path = '/var/lib/prosody/modules',
+  $source = 'https://code.google.com/p/prosody-modules/',
+  $revision = undef,
 ) {
   ensure_packages(['mercurial'])
 
   vcsrepo { $path:
     ensure   => present,
     provider => hg,
-    source   => 'https://code.google.com/p/prosody-modules/',
+    source   => $source,
+    revision => $revision,
     require  => Package['mercurial'],
   }
 }
