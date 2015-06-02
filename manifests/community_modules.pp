818d0a34e17d7a3c0c406a71362902ba7d32c581
Franz Pletz <fpletz@fnordicwalking.de>
2015-06-02 14:12:56 +0200

    Expose more configuration of community modules class

--- manifests/community_modules.pp
+++ manifests/community_modules.pp
@@ -1,13 +1,15 @@
 class prosody::community_modules (
+  $ensure = 'present',
   $path = '/var/lib/prosody/modules',
+  $type = 'hg',
   $source = 'https://code.google.com/p/prosody-modules/',
   $revision = undef,
 ) {
   ensure_packages(['mercurial'])
 
   vcsrepo { $path:
-    ensure   => present,
-    provider => hg,
+    ensure   => $ensure,
+    provider => $type,
     source   => $source,
     revision => $revision,
     require  => Package['mercurial'],
