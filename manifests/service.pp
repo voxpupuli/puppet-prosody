9cf00e7bfa4b31a152cd97df462bd6644c37eb81
Alice Example <alice@example.com>
2015-09-01 15:08:00 -0700

    Add toggle for service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,14 +2,14 @@ class prosody::service {
   case $::osfamily {
     'OpenBSD': {
       service { 'prosody':
-        ensure  => running,
+        ensure  => $daemonize ? running : stopped,
         enable  => true,
         require => Class[prosody::config],
       }
     }
     default: {
       service { 'prosody' :
-        ensure    => running,
+        ensure    => $daemonize ? running : stopped,
         hasstatus => false,
         restart   => '/usr/bin/prosodyctl reload',
         require   => Class[prosody::config],
