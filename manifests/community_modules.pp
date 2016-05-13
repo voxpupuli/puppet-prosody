e877100cfbd26863d7d4776fa6a21eeb968d90a2
toerb <github@toerb.de>
2016-05-13 17:48:02 +0200

    fix module repo source (#38)
    https://prosody.im/community_modules#updating_mercurial_s_repository_url

--- manifests/community_modules.pp
+++ manifests/community_modules.pp
@@ -2,7 +2,7 @@ class prosody::community_modules (
   $ensure = 'present',
   $path = '/var/lib/prosody/modules',
   $type = 'hg',
-  $source = 'https://code.google.com/p/prosody-modules/',
+  $source = 'https://hg.prosody.im/prosody-modules/',
   $revision = undef,
 ) {
   ensure_packages(['mercurial'])
