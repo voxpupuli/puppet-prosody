e3e8e0dbdc3432b8dada700a017ddd571f4a3a2f
gustavo panizzo <gfa@zumbi.com.ar>
2019-12-20 15:43:55 +0100

    Remove class includes using the absolute name

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,17 +41,17 @@ class prosody(
   Optional[String]                        $ssl_protocol = undef,
 ) {
   if ($community_modules != []) {
-    class { '::prosody::community_modules':
+    class { 'prosody::community_modules':
       require => Class['::prosody::package'],
       before  => Class['::prosody::config'],
     }
   }
 
   anchor { 'prosody::begin': }
-  -> class { '::prosody::package': }
-  -> class { '::prosody::config': }
-  -> class { '::prosody::service': }
-  -> anchor { '::prosody::end': }
+  -> class { 'prosody::package': }
+  -> class { 'prosody::config': }
+  -> class { 'prosody::service': }
+  -> anchor { 'prosody::end': }
 
   # create virtualhost resources via hiera
   create_resources('prosody::virtualhost', $virtualhosts, $virtualhost_defaults)
