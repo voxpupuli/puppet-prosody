54f8604ae4d597d07f68c4755c8055ae5d7e2069
Florian Pritz <bluewind@xinu.at>
2021-04-22 17:07:42 +0200

    Manage prosody upstream package repository
    We want the latest and greatest versions (at least of prosody)!
    
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,4 +3,24 @@ class prosody::package {
   package { $prosody::package_name:
     ensure  => $prosody::package_ensure,
   }
+
+  if $prosody::manage_repository {
+    $_keyring_path = '/usr/share/keyrings/prosody.gpg'
+
+    apt::source { 'prosody':
+      comment  => 'Prosody',
+      location => "[signed-by=${_keyring_path}] https://packages.prosody.im/debian",
+      release  => $facts['os']['distro']['codename'],
+      repos    => 'main',
+      before   => Package[$prosody::package_name],
+    }
+
+    file { $_keyring_path:
+      source => $prosody::repo_key,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      before => Package[$prosody::package_name],
+    }
+  }
 }
