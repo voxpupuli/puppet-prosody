61bd42aad84cef416ffc6901219e2a87016c7edb
Niklaus Hofer <niklaus@mykolab.ch>
2021-09-19 18:51:27 +0200

    Allow installation of community modules through Gentoo's Portage.
    Gentoo has the Proosdy community modules packaged, so there is no need to manually manage their installation through Mercurial / Git.

--- manifests/community_modules.pp
+++ manifests/community_modules.pp
@@ -4,20 +4,28 @@ class prosody::community_modules (
   Enum[present, latest] $ensure,
   Stdlib::Absolutepath $path,
   String $source,
-  Enum['hg', 'git'] $type,
+  Enum['hg', 'git', 'os'] $type,
   Optional[String] $revision = undef,
 ) {
   if $type == 'hg' {
     $_packages = ['mercurial']
   } elsif $type == 'git' {
     $_packages = ['git']
+  } elsif $type == 'os' {
+    $_packages = ['net-im/prosody-modules']
   }
 
   ensure_packages($_packages)
-  -> vcsrepo { $path:
-    ensure   => $ensure,
-    provider => $type,
-    source   => $source,
-    revision => $revision,
+  case $type {
+    'portage': {}
+    default:   {
+      vcsrepo { $path:
+        ensure   => $ensure,
+        provider => $type,
+        source   => $source,
+        revision => $revision,
+        require  => Package[ $_packages ],
+      }
+    }
   }
 }
